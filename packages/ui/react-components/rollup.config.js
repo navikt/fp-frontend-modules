340dc1cf46130e0cc222ffbe6395e5ada8c3f24e
tor-nav <tor.roneid@nav.no>
2022-02-04 15:42:10 +0100

    Bytt fra Rollup til Webpack

--- packages/ui/react-components/rollup.config.js
+++
@@ -1,6 +0,0 @@
-import styles from 'rollup-plugin-styles';
-import commonPackageBuild from './../../../config/commonPackageBuild.config';
-
-commonPackageBuild.plugins.unshift(styles());
-
-export default commonPackageBuild;
